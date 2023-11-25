3b5d7ca520be497bf5e233899e92a1ff8a2430c8
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2023-11-25 16:40:19 +0100

    user profile update

--- client/src/components/AllUsers.jsx
+++ client/src/components/AllUsers.jsx
@@ -9,7 +9,7 @@ import '../styles/UserDetails.css'
 import ProgressBar from '@ramonak/react-progress-bar';
 import { BarChart } from '@mui/x-charts/BarChart';
 
-const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collectorFilter}) => {
+const AllUsers = ({ view, changeView, users, fetchUsers, accumulatorFilter, collectorFilter }) => {
     const [clickedUser, setClickedUser] = useState({})
     const columns = [
         { field: 'id', headerName: 'ID', width: 90 },
@@ -25,7 +25,7 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
             width: 150,
             // editable: true,
         },
-        
+
         {
             field: 'fullName',
             headerName: 'Full name',
@@ -33,7 +33,7 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
             sortable: false,
             width: 160,
             valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
         },
         {
             field: 'type',
@@ -42,7 +42,7 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
             // editable: true,
         },
     ];
-    
+
     const handleUserDetails = (user) => {
         users.forEach(row => {
             console.log(row.id);
@@ -53,7 +53,7 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
         changeView('userDetails')
     }
 
-    
+
 
     const renderView = () => {
         if (view === "allusers") {
@@ -72,15 +72,15 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
                         </div>
                     </div>
                     <div className='all-filters' >
-                        <div className='one-filter' onClick={(e)=>{
+                        <div className='one-filter' onClick={(e) => {
                             e.preventDefault();
                             fetchUsers();
                         }} ><span>All Users</span></div>
-                        <div className='one-filter' onClick={(e)=>{
+                        <div className='one-filter' onClick={(e) => {
                             e.preventDefault();
                             collectorFilter()
                         }} ><span>Collector</span></div>
-                        <div className='one-filter' onClick={(e)=>{
+                        <div className='one-filter' onClick={(e) => {
                             e.preventDefault();
                             accumulatorFilter()
                         }} ><span>Accumulator</span></div>
@@ -107,46 +107,20 @@ const AllUsers = ({ view, changeView ,users,fetchUsers,accumulatorFilter,collect
         }
         else if (view === "userDetails") {
             return (
-                <div className='main-section' >
-                    <div className='main-section-body' >
-                        <div className='main-section-body-left'>
-                                <div className='bar-charts-user-details'>
-                                    <h3>Recycling Perspective</h3>
-                                    <BarChart className='bar'
-                                        xAxis={[
-                                            {
-                                                id: 'barCategories',
-                                                data: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-                                                scaleType: 'band',
-                                            },
-                                        ]}
-                                        series={[
-                                            {
-                                                data: [2, 5, 3, 4, 6, 9, 7, 2],
-                                            },
-                                        ]}
-                                        width={500}
-                                        height={300}
-                                        colors={['#73d905']}
-                                    />
-                                </div>
-                                <div className='progress-charts-user-details' >
-                                    <h3>Recycling Progress</h3>
-                                    <div className='one-progress'>
-                                        <h6>Recycling Lectures</h6>
-                                        <ProgressBar completed={70} bgColor='#73d905' className='progress-bars' />
-                                    </div>
-                                    <div className='one-progress'>
-                                        <h6>Recycling Assignments</h6>
-                                        <ProgressBar completed={90} bgColor='#73d905' className='progress-bars' />
-                                    </div>
-                                    
-                                </div>
-                        </div>
-                        <div className='main-section-body-right' >
-                            <OneUser user={clickedUser} />
+                <div>
+                    <div className='overview-top-nav' >
+                        <h1>Users</h1>
+                        <div className='search-section'>
+                            <div className='search-input'>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} className='search-section-icons' />
+                                <input type='text' placeholder='Search' />
+                            </div>
+                            <FontAwesomeIcon icon={faBell} className='search-section-icons' />
+                            <FontAwesomeIcon icon={faEnvelope} className='search-section-icons' />
+                            <FontAwesomeIcon icon={faMessage} className='search-section-icons' />
                         </div>
                     </div>
+                    <OneUser user={clickedUser} changeView={changeView} />
                 </div>
             )
         }
